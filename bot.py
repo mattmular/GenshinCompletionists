a8b989e8fc6cedb9dfa287ca2506a95a6ac21127
BuildTools <unconfigured@null.spigotmc.org>
2022-06-02 03:12:13 -0700

    yes

--- bot.py
+++ bot.py
@@ -1,2 +1,18 @@
-FEATURE = "this is a new feature"
-feature = "ff"
+# Discord and ImageGrab bot for use by Genshin Impact Achievement Mains
+# Written by mistukeni#3065 | @mmitsukeni
+# Created 5 / 30 / 2022
+#
+
+# dependecies
+from PIL import Image, ImageGrab
+
+# constants
+# load numbers
+
+
+
+# def parse info
+    # open image
+    # loop 
+
+
